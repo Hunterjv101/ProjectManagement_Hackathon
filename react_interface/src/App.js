ea8bba9fd079daaf063e462d86d42f875e3e74cb
Jared Button <114765054+jaredb347@users.noreply.github.com>
2023-07-12 16:57:48 -0400

    Updates main page

--- react_interface/src/App.js
+++ react_interface/src/App.js
@@ -2,34 +2,70 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const title = "Pretty Decent Project Management";
-
-function Header() {
-  return <h3>{title}</h3>;
-}
-function Body() {
-  return (
-    <div>
-      <p>View all projects</p>
-      <p>Modify existing project</p>
-      <p>add a project</p>
-      <p>delete a project</p>
-    </div>
-  );
-}
-function Footer() {
-  return (
-    <div>
-      <h4>Empty footer</h4>
-    </div>
-  );
-}
+const footerText = "imagine travelers logo here";
+const divStyle = {
+  backgroundColor: "#FB5050",
+  border: "1px black solid",
+  margin: "10px",
+  padding: "5px",
+  textAlign: "center",
+};
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div
+      className={"boxed"}
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "42%",
+        transform: "translate(0px, -50%)",
+      }}
+    >
+      <Header text={title} />
       <Body />
-      <Footer />
+      <Footer text={footerText} />
+    </div>
+  );
+}
+function vaProj() {
+  alert("Loading project list");
+}
+function maProj() {
+  alert("Loading project modification page");
+}
+function aaProj() {
+  alert("Loading new project input");
+}
+function daProj() {
+  alert("Loading project list to delete");
+}
+
+function Header(props) {
+  return <h3>{props.text}</h3>;
+}
+function Body(props) {
+  return (
+    <div>
+      <button style={divStyle} onClick={vaProj}>
+        View All Projects
+      </button>
+      <button style={divStyle} onClick={maProj}>
+        Modify a Project
+      </button>
+      <button style={divStyle} onClick={aaProj}>
+        Add New Project
+      </button>
+      <button style={divStyle} onClick={daProj}>
+        Delete a project
+      </button>
+    </div>
+  );
+}
+function Footer(props) {
+  return (
+    <div>
+      <h4>{props.text}</h4>
     </div>
   );
 }
