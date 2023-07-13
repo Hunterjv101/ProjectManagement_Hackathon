2ac4993a16f18d0e8499b8b29829d9e2d3c32eba
Jing Li <jingli079@gmail.com>
2023-07-13 10:08:39 -0400

    Go Back Button

--- react_interface/src/Pages/add.jsx
+++ react_interface/src/Pages/add.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function add() {
   return (
     <div id={"add"}>
       <h4>Add Page</h4>
-      
+      <Link to='/'>
+        <button>
+            Go Back
+        </button>
+      </Link>
     </div>
   );
 }
--- react_interface/src/Pages/delete.jsx
+++ react_interface/src/Pages/delete.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function del() {
   return (
     <div id={"delete"}>
       <h4>Delete Page</h4>
-      
+      <Link to='/'>
+        <button>
+            Go Back
+        </button>
+      </Link>
     </div>
   );
 }
