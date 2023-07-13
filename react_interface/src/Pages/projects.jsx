3227d4760a6687a58ebf543541b2081fbde7f7e2
Jared Button <114765054+jaredb347@users.noreply.github.com>
2023-07-13 12:55:38 -0400

    Basic Projects table w/ css

--- react_interface/src/Pages/projects.jsx
+++ react_interface/src/Pages/projects.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./projects.css";
+//import data from mongo
+const data = "";
 
 export default function projects() {
+  //const [data, setData] =  usesState(py_data);
+  // const projects = () => {
   return (
-    <div id={"projects"}>
-      <h4>All Projects Page</h4>
-      <Link to='/'>
-        <button>
-            Go Back
-        </button>
-      </Link>
+    <div className="app-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Project id</th>
+            <th>Team Size</th>
+            <th>Budget</th>
+            <th>Workload</th>
+            <th>Total Experience(yr)</th>
+            <th>Completion Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1(row is hardcoded)</td>
+            <td>1</td>
+            <td>$17</td>
+            <td>Heavy</td>
+            <td>3 years</td>
+            <td>103 weeks</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+// export default projects
